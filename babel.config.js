3c11c0a7cb0f2ad16181e9336470c550d20dd4d6
asnygn <asnygn@gmail.com>
2024-06-13 22:54:53 +0630

    Add path alias config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-};
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@/assets': './src/assets',
+          '@/components': './src/components',
+          '@/contexts': './src/contexts',
+          '@/hooks': './src/hooks',
+          '@/navigation': './src/navigation',
+          '@/screens': './src/screens',
+          '@/stores': './src/stores',
+          '@/styles': './src/styles',
+          '@/themes': './src/themes',
+          '@/types': './src/types',
+          '@/utils': './src/utils',
+        },
+      },
+    ],
+  ],
+}
